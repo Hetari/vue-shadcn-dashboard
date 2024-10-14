bf63b1608ed8c55495d6e66bd0c63134cb418325
Hetari <hetari4py@gmail.com>
2024-10-14 21:10:21 +0300

    fix: import VueFeather

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import './assets/css/main.css';
+import './assets/css/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueFeather from 'vue-feather';
-import PageHeaderVue from './components/ui/PageHeader.vue';
-import Icon from '@/components/ui/Icon.vue';
+import VueFeather from 'vue-feather'
+import PageHeaderVue from './components/ui/PageHeader.vue'
+import Icon from '@/components/ui/Icon.vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.component(VueFeather.name, VueFeather);
-app.component('PageHeader', PageHeaderVue);
-app.component('Icon', Icon);
+app.component(VueFeather.name as string, VueFeather)
+app.component('PageHeader', PageHeaderVue)
+app.component('Icon', Icon)
 app.use(createPinia())
 app.use(router)
 
